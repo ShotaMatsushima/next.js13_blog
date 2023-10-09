f2257df4fd46c33f19cbb50202ebaf73e4f8b788
ShotaMatsushima <shota19890523.engineer@gmail.com>
2023-10-09 19:13:26 +0900

    Add blog component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,17 @@
-export default function Page() {
-  return <h1>Hello, Next.js!</h1>
+import { getAllBlogs } from '@/api'
+import Blogs from './components/Blogs'
+
+export default async function Home() {
+  const blogs = await getAllBlogs();
+
+  return (
+    <main className="flex flex-col items-center bg-blue-500">
+      <div>
+        <h1>Blog page</h1>
+      </div>
+      <div className="w-full px-8 py-6 bg-white shadow-md rounded-lg">
+        <Blogs blogs={blogs} />
+      </div>
+    </main>
+  )
 }
